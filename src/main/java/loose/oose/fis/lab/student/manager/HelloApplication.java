aad4a79eed555afbfc9cb3b17e43da98d72c3352
timiti72 <timiirimia@yahoo.com>
2022-03-30 19:33:55 +0300

    SM-2 Implemented the Student.java file

--- src/main/java/loose/oose/fis/lab/student/manager/HelloApplication.java
+++ src/main/java/loose/oose/fis/lab/student/manager/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.studentmanager;
+package loose.oose.fis.lab.student.manager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/main/java/loose/oose/fis/lab/student/manager/HelloController.java
+++ src/main/java/loose/oose/fis/lab/student/manager/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.studentmanager;
+package loose.oose.fis.lab.student.manager;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
